da1862ad1319929479cb74699301e0f00f7b9b8d
Alexander Ignatovich <cadbimdeveloper@yandex.ru>
2022-09-28 17:38:27 +0300

    auto-formatting

--- NoLitMaterial.ext.js
+++ NoLitMaterial.ext.js
@@ -1,31 +1,31 @@
 class NoLitMaterialExtension extends Autodesk.Viewing.Extension {
 
-    constructor(viewer, options) {
-        super(viewer, options);
-        this.viewer = viewer;
-    }
-    async load() {
-    	if (!this.viewer) return false;
+	constructor(viewer, options) {
+		super(viewer, options);
+		this.viewer = viewer;
+	}
+	async load() {
+		if (!this.viewer) return false;
 
-    	viewer.addEventListener(Autodesk.Viewing.GEOMETRY_LOADED_EVENT, () =>{
+		viewer.addEventListener(Autodesk.Viewing.GEOMETRY_LOADED_EVENT, () => {
 			this.viewer.getSettingsPanel().addCheckbox(
 				"appearance",
 				"Fresnel Reflection",
 				"Turn off all fresnel reflection effects",
 				false,
-				e => {this.toggleCheckbox(e)}
-			);    		
-    	})
-	    return true;
-    }
+				e => { this.toggleCheckbox(e) }
+			);
+		})
+		return true;
+	}
 
-    unload() {
-        return true;
-    }
+	unload() {
+		return true;
+	}
 
-    toggleCheckbox(e) {
-    	const mm = this.viewer.impl.matman()._materials;
-		Object.keys(mm).map( a => { 
+	toggleCheckbox(e) {
+		const mm = this.viewer.impl.matman()._materials;
+		Object.keys(mm).map(a => {
 			if (e) {
 				localStorage.setItem(a, mm[a].reflectivity);
 				mm[a].reflectivity = 0;
@@ -35,7 +35,7 @@ class NoLitMaterialExtension extends Autodesk.Viewing.Extension {
 		})
 
 		this.viewer.impl.invalidate(true);
-    }
+	}
 }
 
 Autodesk.Viewing.theExtensionManager.registerExtension('NoLitMaterialExtension', NoLitMaterialExtension);
